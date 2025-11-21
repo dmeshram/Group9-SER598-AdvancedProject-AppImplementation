49b377b0c9d84a442e5007df594c29c6e189ecef
dmeshram <dmeshram@asu.edu>
2025-11-21 15:25:08 -0700

    Added logout button in the navigation bar

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,9 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from "../auth/AuthContext";
 
 export default function Navigation() {
+    const { isAuthenticated, user, logout } = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    }
     return (
         <Navbar bg='dark' variant='dark' expand='md' fixed='top' className='shadow-sm'>
             <Container>
@@ -12,7 +19,11 @@ export default function Navigation() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                        {!isAuthenticated ? (<Nav.Link href="/login">Login</Nav.Link>) : 
+                        (<>
+                            <Nav.Link disabled>Hi, {user?.name?.split(" ")[0] || "User"}</Nav.Link>
+                            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                        </>)}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
