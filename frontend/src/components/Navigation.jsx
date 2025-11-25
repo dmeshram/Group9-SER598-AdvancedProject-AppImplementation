b0b6c766b82bdad22efd0e4aaf596aba915e9c36
DeepanshuSaxena14 <dsaxen14@asu.edu>
2025-11-25 11:25:45 -0700

    updated Navigation.jsx

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -19,11 +19,14 @@ export default function Navigation() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                        {isAuthenticated ? (<></>) : (<Nav.Link as={Link} to='/landing'>Home</Nav.Link>)}
                         {!isAuthenticated ? (<Nav.Link href="/login">Login</Nav.Link>) : 
                         (<>
                             <Nav.Link disabled>Hi, {user?.name?.split(" ")[0] || "User"}</Nav.Link>
+                            <Nav.Link as={Link} to='/'>Home</Nav.Link>
                             <Nav.Link as={Link} to='/profile'>Profile</Nav.Link>
                             <Nav.Link as={Link} to='leaderboard'>Leaderboard</Nav.Link>
+                            <Nav.Link as={Link} to='achievements'>Achievements</Nav.Link>
                             <Nav.Link as={Link} to='history'>History</Nav.Link>
                             <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
                         </>)}
